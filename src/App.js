6514c4de6c8636cbe8c444cfd7a2d534294f3e5a
Bexultan Myrzatayev <a.e.iimitk@gmail.com>
2018-06-26 16:12:54 +0600

    Gold medal earned

--- src/App.js
+++ src/App.js
@@ -1,18 +1,113 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      bgColor: '',
+      textColor: '',
+      points: 0,
+      lives: 3
+    }
+  }
+
+  componentDidMount = () => {
+    this.setColors();
+  }
+
+  setColors = () => {
+    const colors = ['yellow', 'green', 'blue', 'red', 'black'];
+    this.setState({
+      bgColor: colors[Math.floor(Math.random()*colors.length)],
+      textColor: colors[Math.floor(Math.random()*colors.length)]
+    })
+  }
+
+  componentDidUpdate = () => {
+    if (this.state.points === 10) {
+      alert("Hooray!");
+      this.reset();
+    }
+
+    if (this.state.lives === 0) {
+      alert("Game Over");
+      this.reset();
+    }
+  }
+
+  reset = () => {
+    this.setState({
+      lives: 3,
+      points: 0
+    })
+  }
+
+  trueClicked = () => {
+    if (this.state.bgColor === this.state.textColor) {
+      this.setState({
+        points: this.state.points + 1
+      })
+    } else {
+      this.setState({
+        lives: this.state.lives - 1 
+      })
+    }
+    this.setColors();
+  }
+
+  falseClicked = () => {
+    if (this.state.bgColor !== this.state.textColor) {
+      this.setState({
+        points: this.state.points + 1
+      })
+    } else {
+       this.setState({
+        lives: this.state.lives - 1 
+      })
+    }
+    this.setColors();
+  }
+
+  Lives = () => {
+    if (this.state.lives !== 1) {
+      return <p>You have {this.state.lives} lives</p>;
+    } else {
+      return <p>You have {this.state.lives} life</p>;
+    }
+  }
+
+  Points = () => {
+    if (this.state.points !== 1) {
+      return <p>{this.state.points} points</p>;
+    } else {
+      return <p>{this.state.points} point</p>;
+    }
+  }
+
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div className="colorBox" style={{ backgroundColor: this.state.bgColor }}>
+          <h3 className="colorText">{this.state.textColor}</h3>
+        </div>
+
+        <button onClick={() => {this.trueClicked()}}>
+          True
+        </button>
+
+        <button onClick={() => {this.falseClicked()}}>
+          False
+        </button>
+
+        {this.Lives()}
+
+        {this.Points()}
+
+
       </div>
     );
   }
